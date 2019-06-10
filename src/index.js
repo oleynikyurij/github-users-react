53b898b7bd33ee9b301c67dfae4e0490380c8d5b
unknown <D:\\Mail>
2019-06-10 19:25:06 +0300

    token

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {	ApolloClient} from 'apollo-client';
 import {	HttpLink} from 'apollo-link-http';
 import {	InMemoryCache} from 'apollo-cache-inmemory';
 
-const token = "42af8914c8735ab3a7dcc131500ea4488fe3436e";
+const token = "1279fc0222d2d85581eb26d173bdf7f3f447f474";
 
 const httpLink = {
 	uri: 'https://api.github.com/graphql',
